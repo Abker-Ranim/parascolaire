c4112f888f78501999122ece5284d7de351980ab
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2024-12-21 14:11:39 +0100

    add  a valid email address

--- frontend/src/app/club/club.component.ts
+++ frontend/src/app/club/club.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import de CommonModule
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -7,15 +7,15 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 @Component({
   selector: 'app-clubs',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgxSpinnerModule], // Assurez-vous d'importer CommonModule
+  imports: [CommonModule, FormsModule, NgxSpinnerModule],
   templateUrl: './club.component.html',
   styleUrls: ['./club.component.css'],
 })
 export class ClubComponent implements OnInit {
-  userRole: string = ''; // Le rôle de l'utilisateur (admin, club, student, membre)
+  userRole: string = '';
 
   showForm = false;
-  clubCards: any[] = []; // Tableau pour stocker les cartes des clubs
+  clubCards: any[] = [];
   newClub = {
     name: '',
     email: '',
@@ -24,23 +24,24 @@ export class ClubComponent implements OnInit {
     description: '',
     imageUrl: '',
   };
-  selectedClub: any; // Variable pour stocker les informations du club sélectionné
-
-  constructor(private router: Router, private route: ActivatedRoute, private spinner: NgxSpinnerService) {}
+  selectedClub: any;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit() {
     this.userRole = localStorage.getItem('userRole') || 'membre';
-    // Charger les cartes depuis le LocalStorage si elles existent
     const savedCards = localStorage.getItem('clubCards');
     if (savedCards) {
       this.clubCards = JSON.parse(savedCards);
     }
 
-    // Récupérer l'ID du club depuis les paramètres de l'URL (si existe)
     this.route.params.subscribe((params) => {
       const clubId = params['clubId'];
       if (clubId) {
-        this.selectedClub = this.clubCards.find((club) => club.id === +clubId); // Récupère le club par ID
+        this.selectedClub = this.clubCards.find((club) => club.id === +clubId);
       }
     });
   }
@@ -50,7 +51,6 @@ export class ClubComponent implements OnInit {
   }
 
   onSubmit() {
-    // Vérifier que tous les champs obligatoires sont remplis
     this.spinner.show();
     setTimeout(() => {
       if (
@@ -63,15 +63,16 @@ export class ClubComponent implements OnInit {
         alert('Please fill in all required fields!');
         return;
       }
-  
-      // Ajout de la nouvelle carte au tableau des cartes de club
-      const newClubWithId = { ...this.newClub, id: Date.now() }; // Utiliser Date.now() pour générer un ID unique
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(this.newClub.email)) {
+        alert('Please enter a valid email address!');
+        return;
+      }
+      const newClubWithId = { ...this.newClub, id: Date.now() };
       this.clubCards.push(newClubWithId);
-  
-      // Sauvegarder les cartes dans le LocalStorage
+
       localStorage.setItem('clubCards', JSON.stringify(this.clubCards));
-  
-      // Réinitialisation du formulaire
+
       this.newClub = {
         name: '',
         email: '',
@@ -81,8 +82,8 @@ export class ClubComponent implements OnInit {
         imageUrl: '',
       };
       this.showForm = false;
-      this.spinner.hide(); // Ferme le formulaire après la soumission
-    },500);
+      this.spinner.hide();
+    }, 500);
   }
 
   onFileChange(event: any) {
@@ -90,13 +91,12 @@ export class ClubComponent implements OnInit {
     if (file) {
       const reader = new FileReader();
       reader.onload = (e: any) => {
-        this.newClub.imageUrl = e.target.result; // Enregistre l'image en base64
+        this.newClub.imageUrl = e.target.result;
       };
       reader.readAsDataURL(file);
     }
   }
 
-  // Méthode pour naviguer vers la page de profil d'un club lorsqu'on clique sur une carte
   navigateToClubProfile(clubId: number) {
     this.router.navigate([`/${this.userRole}/club/${clubId}`]);
   }
